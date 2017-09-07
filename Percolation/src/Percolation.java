b885fb1ca26e5570746fbb81678a9b9fe1adda76
kribblez <31427888+kribblez@users.noreply.github.com>
2017-09-07 12:02:21 -0600

    First File Upload

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,42 @@
+/*
+ * Kirk Kirkland 
+ * Algorithms and Data Structures
+ * A01 - Percolation
+ */
+
+/**
+ *
+ * @author kbkla
+ */
+
+
+public class Percolation {
+
+    public Percolation(int n) {
+        
+        // TODO create N-by-N grid, all sites blocked
+    }
+    
+    public void open(int i, int j) {
+        
+        // TODO open site
+        
+    }
+    
+    public boolean isOpen(int i, int j) {
+        
+        // TODO is open?
+    }
+    
+    public boolean isFull(int i, int j) {
+        
+        // TODO is full?
+        
+    }
+    
+    public boolean percolates() {
+        
+        //TODO does the system percolate
+    }
+    
+}
---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,46 @@
+/*
+ * Kirk Kirkland // Jordan // 
+ * Algorithms and Data Structures
+ * A01 - Percolation
+ */
+
+/**
+ *
+ * @author kbkla
+ */
+
+import java.lang.Math;
+
+public class PercolationStats {
+
+    public PercolationStats(int n, int t) {
+        
+        // TODO perform t experiments on N-by-N grid
+        
+    }
+    
+    
+    public double mean() {
+        
+        //TODO // sample mean of percolation threshold
+        
+    }
+    
+    public double stddiv() {
+        
+        // TODO // sample standard deviation of percolation threshold
+        
+    }
+    
+    public double confidenceLow() {
+        
+        //TODO / low  endpoint of 95% confidence interval
+        
+    }
+    
+    public double confidenceHigh() {
+        
+        //TODO // high endpoint of 95% confidence interval
+        
+    }
+}
